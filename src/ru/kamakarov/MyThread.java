9d9097c4fcf0aeb7c36324eb4d9535484956fb53
Kirill Makarov <kir-maka@yandex.ru>
2016-02-11 00:51:09 +0300

    add gitignore

--- src/ru/kamakarov/MyThread.java
+++ src/ru/kamakarov/MyThread.java
@@ -2,7 +2,23 @@ package ru.kamakarov;
 
 public class MyThread extends Thread {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World 1.");
+    static int i = 0;
+
+    @Override
+    public void run() {
+        for (int j = 0; j<5; j++) {
+           if (i==4) System.out.println(i);
+        }
+
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(2222);
+        for (int j = 0; j < 500000; j++) {
+            (new MyThread()).start();
+
+            i++;
+        }
     }
 }
